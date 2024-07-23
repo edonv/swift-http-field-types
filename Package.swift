1675d4d07920171a6f68469ce1c2cbc8b26d53a5
Edon <edon@valdman.works>
2024-07-23 15:52:01 -0500

    Added `swift-http-types` as a dependency

--- Package.swift
+++ Package.swift
@@ -12,15 +12,24 @@ let package = Package(
             targets: ["HTTPRanges"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HTTPRanges"
+            name: "HTTPRanges",
+            dependencies: [
+                .product(name: "HTTPTypes", package: "swift-http-types"),
+            ]
         ),
         .testTarget(
             name: "HTTPRangesTests",
-            dependencies: ["HTTPRanges"]
+            dependencies: [
+                "HTTPRanges",
+                .product(name: "HTTPTypes", package: "swift-http-types"),
+            ]
         ),
     ]
 )
