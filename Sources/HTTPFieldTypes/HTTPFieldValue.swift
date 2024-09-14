c626a887281ad529e47211148c9a3d22baf60084
Edon <edon@valdman.works>
2024-09-14 15:08:46 -0500

    Update HTTPFieldValue.swift
    Added explicit conformance of `Equatable` and `Hashable` for `HTTPFieldContent`, and added a note about it to docs.

--- Sources/HTTPFieldTypes/HTTPFieldValue.swift
+++ Sources/HTTPFieldTypes/HTTPFieldValue.swift
@@ -12,6 +12,8 @@ import HTTPTypes
 //public enum HTTPFieldValues {}
 
 /// A protocol for any type that can be represented as the `String` value of an HTTP header field.
+///
+/// > Important: Because all instances of conforming types are intended for use in the context of HTTP headers as strings, all equality checks of conforming types will use their `String` form (``fieldContent-2nxzz``).
 public protocol HTTPFieldContent: Hashable, Sendable, CustomStringConvertible {
     /// The value in string form, for use in a header field.
     var fieldContent: String { get }
@@ -21,6 +23,14 @@ public protocol HTTPFieldContent: Hashable, Sendable, CustomStringConvertible {
 
 extension HTTPFieldContent {
     public var description: String { fieldContent }
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.fieldContent == rhs.fieldContent
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(fieldContent)
+    }
 }
 
 extension HTTPFieldContent where Self: RawRepresentable, RawValue == String {
